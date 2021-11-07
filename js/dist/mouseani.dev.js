eb4337319e71d756d50ff7595b4ac310c0c1a9c6
Jasper <48677701+jasperFernando@users.noreply.github.com>
2021-11-07 13:46:16 +0530

    updated cursor

--- js/dist/mouseani.dev.js
+++ js/dist/mouseani.dev.js
@@ -2,7 +2,7 @@
 
 var cursor = document.querySelector(".cursor");
 document.addEventListener("mousemove", function (e) {
-  TweenMax.setAttribute("style", "top: " + (e.pageY - 40) + "px; left: " + (e.pageX - 40) + "px;");
+  cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;");
 });
 document.addEventListener("click", function (e) {
   console.log(e.target);
@@ -10,22 +10,21 @@ document.addEventListener("click", function (e) {
   setTimeout(function () {
     cursor.classList.remove("click");
   }, 500);
-});
-var btn1 = document.querySelector(".btn1");
-var btn2 = document.querySelector(".btn2");
-var btn3 = document.querySelector(".btn3");
-btn1.addEventListener("click", function () {
-  cursor.classList.remove("style2");
-  cursor.classList.remove("style3");
-  cursor.classList.add("style1");
-});
-btn2.addEventListener("click", function (e) {
-  cursor.classList.remove("style1");
-  cursor.classList.remove("style3");
-  cursor.classList.add("style2");
-});
-btn3.addEventListener("click", function (e) {
-  cursor.classList.remove("style1");
-  cursor.classList.remove("style2");
-  cursor.classList.add("style3");
-});
+}); // const btn1 = document.querySelector(".btn1");
+// const btn2 = document.querySelector(".btn2");
+// const btn3 = document.querySelector(".btn3");
+// btn1.addEventListener("click", function () {
+//   cursor.classList.remove("style2");
+//   cursor.classList.remove("style3");
+//   cursor.classList.add("style1");
+// });
+// btn2.addEventListener("click", (e) => {
+//   cursor.classList.remove("style1");
+//   cursor.classList.remove("style3");
+//   cursor.classList.add("style2");
+// });
+// btn3.addEventListener("click", (e) => {
+//   cursor.classList.remove("style1");
+//   cursor.classList.remove("style2");
+//   cursor.classList.add("style3");
+// });
--- js/mouseani.js
+++ js/mouseani.js
@@ -1,38 +1,41 @@
 const cursor = document.querySelector(".cursor");
-    document.addEventListener("mousemove", (e) => {
-      TweenMax.setAttribute(
-        "style",
-        "top: " + (e.pageY - 40) + "px; left: " + (e.pageX - 40) + "px;"
-      );
-    });
+document.addEventListener("mousemove", (e) => {
+  cursor.setAttribute(
+    "style",
+    "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;"
+  );
+});
 
-    document.addEventListener("click", (e) => {
-      console.log(e.target);
-      cursor.classList.add("click");
+document.addEventListener("click", (e) => {
+  console.log(e.target);
+  cursor.classList.add("click");
 
-      setTimeout(() => {
-        cursor.classList.remove("click");
-      }, 500);
-    });
 
-    const btn1 = document.querySelector(".btn1");
-    const btn2 = document.querySelector(".btn2");
-    const btn3 = document.querySelector(".btn3");
+  setTimeout(() => {
+    cursor.classList.remove("click");
+  }, 500);
+});
 
-    btn1.addEventListener("click", function () {
-      cursor.classList.remove("style2");
-      cursor.classList.remove("style3");
-      cursor.classList.add("style1");
-    });
+// const btn1 = document.querySelector(".btn1");
+// const btn2 = document.querySelector(".btn2");
+// const btn3 = document.querySelector(".btn3");
 
-    btn2.addEventListener("click", (e) => {
-      cursor.classList.remove("style1");
-      cursor.classList.remove("style3");
-      cursor.classList.add("style2");
-    });
 
-    btn3.addEventListener("click", (e) => {
-      cursor.classList.remove("style1");
-      cursor.classList.remove("style2");
-      cursor.classList.add("style3");
-    });
+
+// btn1.addEventListener("click", function () {
+//   cursor.classList.remove("style2");
+//   cursor.classList.remove("style3");
+//   cursor.classList.add("style1");
+// });
+
+// btn2.addEventListener("click", (e) => {
+//   cursor.classList.remove("style1");
+//   cursor.classList.remove("style3");
+//   cursor.classList.add("style2");
+// });
+
+// btn3.addEventListener("click", (e) => {
+//   cursor.classList.remove("style1");
+//   cursor.classList.remove("style2");
+//   cursor.classList.add("style3");
+// });
