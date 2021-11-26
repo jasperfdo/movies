c493d308656a010324023bb5a1d2b33b95be83f0
Jasper <48677701+jasperFernando@users.noreply.github.com>
2021-11-27 00:07:20 +0530

    updated

--- main.js
+++ main.js
@@ -5,25 +5,25 @@
 const locoScroll = new LocomotiveScroll({
 	el: document.querySelector(".scrollContainer"),
 	smooth: true
-  }); 
-  
-  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-  
-  
-  
-		 locoScroll.on("scroll", ScrollTrigger.update);
-  
-  // tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
-	  ScrollTrigger.scrollerProxy(".scrollContainer", {
-			scrollTop(value) {
-				  return arguments.length ? locoScroll.scrollTo(value, 0, 0) : 		locoScroll.scroll.instance.scroll.y;
+});
+
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+
+
+
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy(".scrollContainer", {
+	scrollTop(value) {
+		return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
 	}, // we don't have to define a scrollLeft because we're only scrolling vertically.
 	getBoundingClientRect() {
-	  return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+		return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
 	},
 	// LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
 	pinType: document.querySelector(".scrollContainer").style.transform ? "transform" : "fixed"
-  });
+});
 
 
 //gsap
@@ -32,16 +32,31 @@ const locoScroll = new LocomotiveScroll({
 // gsap.fromTo('.imgg', {opacity:0, duration:4, x:150, ease:'Power2.easeInOut'}, {delay: .5, opacity:1, duration:2, x:-80, ease:'Power2.easeInOut'})
 // gsap.fromTo('.header', {opacity:0, duration:0, y:-20, ease:'Power2.easeInOut'}, {delay: 1, opacity:1, duration:1, y:0, ease:'Power2.easeInOut'})
 
-var tl = gsap.timeline({defaults:{duration: 1}})
+var tl = gsap.timeline({ defaults: { duration: 1 } })
 
-  tl.to(".htanim", {opacity:1, y:-50, ease:'Power2.easeInOut', stagger: .2})
-  	.to(".herowrk", {opacity:1, y:-20, ease:'Power2.easeInOut'}, "-=.5")
-  	.fromTo(".imgg", {opacity:0, x:150, ease:'Power2.easeInOut'}, {opacity:1, x:-74, ease: "Expo.easeOut"}, "-=.5")
-  	.fromTo(".header", {opacity:0, y:-20, ease:'Power5'}, { opacity:1, y:0, ease:'Power5'}, "-=1.4")
-
-
+tl.to(".htanim", { opacity: 1, y: -50, ease: 'Power2.easeInOut', stagger: .2 })
+	.to(".herowrk", { opacity: 1, y: -20, ease: 'Power2.easeInOut' }, "-=.5")
+	.fromTo(".imgg", { opacity: 0, x: 150, ease: 'Power2.easeInOut' }, { opacity: 1, x: -74, ease: "Expo.easeOut" }, "-=.5")
+	.fromTo(".header", { opacity: 0, y: -20, ease: 'Power5' }, { opacity: 1, y: 0, ease: 'Power5' }, "-=1.4")
 
 
+
+//Scrolltrigger with loco with gsap
+
+let wrtl = gsap.timeline({defaults: {}})
+	.fromTo(".worktitle, .char", {y:-50, opacity: 0, duration: 1}, {y:0, opacity: 1, ease: "Power1.easeInOut"})
+	.fromTo(".word", {y:-50, opacity: 0, duration: 1}, {y:0, opacity: 1, ease: "slow.easeOut", stagger: 0.050},"-=1.5")	
+
+
+
+ScrollTrigger.create( {
+	trigger: '.worktitle',
+	start: "top bottom",
+	end: "+=300",
+	scroller: ".scrollContainer",
+	animation:wrtl
+
+})
 //gsap
 
 // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
